17748fb62040bddd2c39dc01806b4033afccd67c
aanthr0 <anthromadayt@gmail.com>
2021-06-01 21:53:48 +0200

    Set up the bot basics

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,7 @@ module.exports = async (client) => {
     console.clear();
     
     console.log(`-----------------------`);
-    console.log(`${client.user.username}`);
+    console.log(`${client.user.username} v${require("../../package.json").version}`);
     console.log(`Logged into Discord and ready to use!`);
     console.log(`-----------------------`);
     
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 const { Client, Collection } = require("discord.js");
 const client = new Client({ disableMentions: "everyone" });
+const mongoose = require("mongoose");
 
 client.config = require("./config.js");
 ["commands", "aliases"].forEach(x => client[x] = new Collection());
 
 require("./handlers/command.js")(client);
 require("./handlers/event.js")(client);
+
+mongoose.connect(client.config.database.uri, {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Connected to MongoDB!");
+}).catch(err => {
+    console.log(err);
+});
 
 client.login(client.config.token);
