61af752e5a92100b26c2dbf149022c7943122c60
aanthr0 <anthromadayt@gmail.com>
2021-06-07 15:43:39 +0200

    can't figure out upload err

--- src/commands/general/upload.js
+++ src/commands/general/upload.js
@@ -1,4 +1,4 @@
-const configSchema = require("../../schemas/configSchema.js");
+const userSchema = require("../../schemas/userSchema.js");
 const { MessageEmbed } = require("discord.js");
 const { createWriteStream, unlink } = require("fs");
 const { join } = require("path");
@@ -34,7 +34,7 @@ module.exports.run = async(client, message, args) => {
                 .setFooter(footer)
             );
             return;
-        }
+        };
         attachment = attachments.first().url;
     }
     else if(args[0].startsWith("http")) attachment = args[0];
@@ -55,8 +55,8 @@ module.exports.run = async(client, message, args) => {
     for(const filetype of filetypes) {
         if(attachment.endsWith(filetype) !== -1) {
             filename = attachment.split("/");
-            filelocation = `\\temp\\${filename[filename.length-1]}`;
-            filename = `./temp/${filename[filename.length-1]}`;
+            filelocation = `\\images\\${filename[filename.length-1]}`;
+            filename = `./images/${filename[filename.length-1]}`;
             filelocation = join(__dirname, `..\\..\\..\\${filelocation}`);
             break;
         }
@@ -94,7 +94,7 @@ module.exports.run = async(client, message, args) => {
                         .setColor(color.negative)
                         .setAuthor(author.tag, author.displayAvatarURL({ format: "png", dynamic: true, size: 1024 }))
                         .setTitle(`${emoji.negative} Error!`)
-                        .addField("Image Too Big", `You cannot upload images larger than \`95 megabytes (approx. 90.59 MiB)\` to [sxcu.net](https://sxcu.net/api/docs/endpoint/img_upload)!`)
+                        .addField("Image Too Big", `You cannot upload images larger than \`95 MB (approx. 90.59 MiB)\` to [sxcu.net](https://sxcu.net/api/docs/endpoint/img_upload)!`)
                         .setFooter(footer)
                     );
                     return;
@@ -105,48 +105,78 @@ module.exports.run = async(client, message, args) => {
         );
     };
 
-    if(!await configSchema.findById(author.id)){
+    if(!await userSchema.findById(author.id)){
         channel.send(
             new MessageEmbed()
             .setColor(color.negative)
             .setAuthor(author.tag, author.displayAvatarURL({ format: "png", dynamic: true, size: 1024 }))
             .setTitle(`${emoji.negative} Error!`)
-            .setDescription(`Before using this command, you need to set up your domain!\nTo do that, please run \`${prefix}setup\` in my DMs.`)
+            .setDescription(`Before using this command, you need to set up your domain!\nIf you don't already have a domain, [click here](https://sxcu.net/).\nWith your domain, run \`${prefix}setup <subdomain> [token (if the subdomain is private)]\` in my DMs.`)
             .setFooter(footer)
         );
         return;
     };
-    let { url, token } = await configSchema.findById(author.id);
-    const { attachFile, upload } = new Image(url, token);
-    
-    download(attachment, filename, () => {})
-    
-    setTimeout(() => {
-        attachFile(filelocation);
-    }, 3000);
 
-    upload().then(img => {
-        channel.send(
-            new MessageEmbed()
-            .setColor(color.positive)
-            .setAuthor(author.tag, author.displayAvatarURL({ format: "png", dynamic: true, size: 1024 }))
-            .setTitle(`${emoji.positive} Success!`)
-            .setDescription(`Your image has been uploaded successfully!\n\nImage URL: ${img.body.url}\nDeletion URL: ${img.body.del_url}`)
-            .setThumbnail(img.body.thumb)
-            .setFooter(footer)
-        );
-    }).catch(err => {
-        const error = new MessageEmbed()
-            .setColor(color.negative)
-            .setAuthor(author.tag, author.displayAvatarURL({ format: "png", dynamic: true, size: 1024 }))
-            .setTitle(`${emoji.negative} Error!`)
-            .setFooter(footer);
-        if(err.reason) console.log("brr"); /* error.setDescription("This subdomain is private!\nPlease run the setup command in my DMs, but provide a valid access token!"); */
+    let { url, token } = await userSchema.findById(author.id);
+    const { attachFile, upload } = new Image(url, token);
+
+    download(attachment, filename, () => {
+        attachFile(filelocation);
+        upload().then(img => {
+            channel.send(
+                new MessageEmbed()
+                    .setColor(color.positive)
+                    .setAuthor(author.tag, author.displayAvatarURL({ format: "png", dynamic: true, size: 1024 }))
+                    .setTitle(`${emoji.positive} Success!`)
+                    .setDescription(`Your image has been uploaded successfully!\n\nImage URL: ${img.body.url}\nDeletion URL: ${img.body.del_url}`)
+                    .setThumbnail(img.body.thumb)
+                    .setFooter(footer)
+                );
+            }).catch(err => {
+                const error = new MessageEmbed()
+                    .setColor(color.negative)
+                    .setAuthor(author.tag, author.displayAvatarURL({ format: "png", dynamic: true, size: 1024 }))
+                    .setTitle(`${emoji.negative} Error!`)
+                    .setFooter(footer);
+                
+                if(err.reason) {
+                    error.setDescription("This subdomain is private!\nPlease run the setup command in my DMs, but provide a valid access token!");
+                    channel.send(error);
+                }
+            });
+        }
+    );
+        setTimeout(() => unlink(filelocation, err => { if(err) console.log(err); }), 2500);
+    
+    setTimeout(async() => {
+        const date = new Date();
+        date.setMonth(date.getMonth() + 1);
+        date.setHours(0, 0, 0, 0);
         
-        // channel.send(error);
-    }).finally(() => {
-        unlink(filelocation, (err) => { if(err) console.log(err); });
-    });
+        await userSchema.findByIdAndUpdate(
+            author.id,
+            {
+                expires: date
+            },
+            {
+                upsert: true,
+            },
+            (err, doc, res) => {
+                if(err) {
+                    channel.send(
+                        new MessageEmbed()
+                        .setColor(color.negative)
+                        .setAuthor(author.tag, author.displayAvatarURL({ format: "png", dynamic: true, size: 1024 }))
+                        .setTitle(`${emoji.negative} Error!`)
+                        .setDescription("An internal error has occured, please try again later.")
+                        .setFooter(footer)
+                    );
+                    console.log(err);
+                    return;
+                };
+            }
+        );
+    }, 3500);
 };
 
 module.exports.help = {
